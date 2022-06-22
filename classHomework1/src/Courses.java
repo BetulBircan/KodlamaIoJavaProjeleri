971783b2412965ad82a228dd95b57b9c78754acc
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-06-23 00:41:40 +0300

    kurslara ait özell,kleri belirten class yapısı ve contructor yapıları olusturuldu

---
+++ classHomework1/src/Courses.java
@@ -0,0 +1,14 @@
+public class Courses {
+
+    public Courses(int id, String name, String lessons, String instructor) {
+        this.id = id;
+        this.name = name;
+        this.lessons = lessons;
+        this.instructor = instructor;
+    }
+
+    int id;
+    String name;
+    String lessons;
+    String instructor;
+}
--- classHomework1/src/Main.java
+++ classHomework1/src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Courses course1 = new Courses(1, "Yazılım Geliştirme Kampı(JAVA + REACT)", "15 DERS",
+                "ENGİN DEMİROĞ" );
+
+        Courses course2 = new Courses(2, "Yazılım Geliştirme Kampı(JavaScript)", "4 DERS",
+                "ENGİN DEMİROĞ");
+
+        Courses course3 = new Courses(3, "Yazılım Geliştirme Kampı(C# + ANGULAR)", "18 DERS",
+                "ENGİN DEMİROĞ");
+
+        Courses course4 = new Courses(4, "Proglamaya Giriş İçin Temel Kurs", "7 DERS",
+                "ENGİN DEMİROĞ");
+
+        Courses[] courses = {course1, course2, course3, course4};
+
+        for (Courses course: courses) {
+            System.out.println("Kurs ID: " + course.id + "  " +  "Kurs Adı: " +  course.name
+                    + "  " + "Ders: " +course.lessons + "  " + "Eğitmen: " + course.instructor
+            );
+        }
+
     }
 }
