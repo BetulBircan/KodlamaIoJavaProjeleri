3803804ebacab4c25c43ffd547e299e2c5e19c89
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-07-17 20:43:15 +0300

    StudentManager sınıfı eklendi

---
+++ inheritanceHomework2/src/StudentManager.java
@@ -0,0 +1,5 @@
+public class StudentManager extends UserManager{
+    public void addCourse(Student student) {
+        System.out.println(student.fullName + "  " + "Kurs Ekleme İşlemi Başarılı!");
+    }
+}
