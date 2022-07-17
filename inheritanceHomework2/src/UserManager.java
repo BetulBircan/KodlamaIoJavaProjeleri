a1dc0d70de0c8ffd113faf91b73bf6b99caed85e
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-07-17 20:38:00 +0300

    UserManager sınıfı eklendi

---
+++ inheritanceHomework2/src/UserManager.java
@@ -0,0 +1,13 @@
+public class UserManager {
+    public void add() {
+        System.out.println("Ekleme İşlemi Başarılı!");
+    }
+
+    public void remove() {
+        System.out.println("Silme İşlemi Başarılı!");
+    }
+
+    public void update() {
+        System.out.println("Güncelleme İşlemi Başarılı!");
+    }
+}
